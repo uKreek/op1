966dcc3b1c7fcf5d23014fe34bd6058813c6a95a
uKreek <134139003+uKreek@users.noreply.github.com>
2024-09-17 00:38:37 +0200

    Лаба 1
    add: типы данных

---
+++ lab_1/main.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+int main()
+{
+	 std::cout << "char: 8 bit [-128, 127] or [0, 255]" << std::endl;
+	 std::cout << "short: 16 bit [-32768, 32767]" << std::endl;
+	 std::cout << "int: 16 or 32 bit [-32768, 32767] or [-2147483648, 2147483647]" << std::endl;
+	 std::cout << "long: 32 or 64 bit [-2147483648, 2147483647] or [-9223372036854775, 9223372036854774]" << std::endl;
+	 std::cout << "bool: True or False" << std::endl;
+	 std::cout << "float: 32 bit [+/- 3.4E-38, 3.4E+38]" << std::endl;
+	 std::cout << "double: 64 bit [+/- 1.7E-308, 1.7E+308]" << std::endl;
+	 std::cout << "" << std::endl;
+	 std::cout << "int: 4 + 5 = " << 4+5 << std::endl;
+	 std::cout << "float: 0.2 * 50 = " << 0.2*50 << std::endl;
+
+
+	return 0;
+}
